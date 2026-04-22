5bd0ec5ec2c74a74bbb29521c9fad10b9a60f3bd
SophiaBelkhir <sophia.belkhir@ens-lyon.fr>
2026-04-22 15:36:35 +0100

    add report

--- run_stage3.sh
+++ run_stage3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ANALYSIS_DIR="/lustre/scratch126/casm/staging/team267_murchison/sb71/devils_high_cov"
 PIPELINE_DIR="/lustre/scratch126/casm/staging/team267_murchison/sb71/mutect2_pipeline"
 echo "Launch time: $(date)"
@@ -11,6 +12,7 @@ nextflow run ${PIPELINE_DIR}/stage3.nf \
          --panel_of_normals ${ANALYSIS_DIR}/03_04_26_mutect2_stage2_test/MergedResources/panel_of_normals.vcf.gz \
          --somatic_candidates ${ANALYSIS_DIR}/03_04_26_mutect2_stage2_test/MergedResources/candidates.vcf.gz \
          --intervals 99 \
-         --outdir ${ANALYSIS_DIR}/mutect_stage3_outputs \
+         --outdir /nfs/casm/team267_murchison/sb71 \
+         -with-report \ 
          -resume \
          -config ${PIPELINE_DIR}/stage3_nextflow.config
