61cba788ef6a98440da110f9425c6b15ddc4dac7
SophiaBelkhir <sophia.belkhir@ens-lyon.fr>
2026-06-17 15:46:34 +0100

    uncomment to run

--- stage2_scripts/merge_somatic_candidates.sh
+++ stage2_scripts/merge_somatic_candidates.sh
@@ -2,14 +2,14 @@
 
 PYSCRIPT="/lustre/scratch126/casm/staging/team267_murchison/sb71/mutect2_pipeline/stage2_scripts/fix_tumor_sample_in_somatic_candidates_vcf_header.py"
 
-#for vcf in *.somatic_candidates.vcf.gz; do
-#    echo "Indexing $vcf..."
-#    bcftools index -f -t "$vcf"
-#done
+for vcf in *.somatic_candidates.vcf.gz; do
+    echo "Indexing $vcf..."
+    bcftools index -f -t "$vcf"
+done
 
-#echo "Indexed all somatic candidate VCFs. Now concatenating..."
-#bcftools concat -a -D *.vcf.gz | bcftools sort -Oz -o somatic_candidates_pre_fix.vcf.gz
-#echo "Concatenated somatic candidates VCF created: somatic_candidates_pre_fix.vcf.gz"
+echo "Indexed all somatic candidate VCFs. Now concatenating..."
+bcftools concat -a -D *.vcf.gz | bcftools sort -Oz -o somatic_candidates_pre_fix.vcf.gz
+echo "Concatenated somatic candidates VCF created: somatic_candidates_pre_fix.vcf.gz"
 bcftools index -t somatic_candidates_pre_fix.vcf.gz
 $PYSCRIPT somatic_candidates_pre_fix.vcf.gz somatic_candidates.vcf.gz
 echo "Fixed tumor sample name in VCF header. Output written to somatic_candidates.vcf.gz"
